8bbe6c904ffa3e2db102589d2f4611743c4a60c2
Konstantin S <havendv@gmail.com>
2020-08-09 19:24:52 +1000

    Added IpInfoApi constructor with token.

---
+++ src/IpInfo/IpInfoApi.Custom.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace IpInfo
+{
+    /// <summary>
+    /// Class providing methods for API access.
+    /// </summary>
+    public partial class IpInfoApi
+    {
+        /// <summary>
+        /// Sets the selected token as a default header for the HttpClient.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="httpClient"></param>
+        public IpInfoApi(string token, HttpClient httpClient) : this(httpClient)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+        }
+    }
+}
