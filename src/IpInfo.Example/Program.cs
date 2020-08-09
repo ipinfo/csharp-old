7408d8926ed3aeaa6423a71a51ba072df941939d
Konstantin S <havendv@gmail.com>
2020-08-10 09:40:53 +1000

    Added example project.

---
+++ src/IpInfo.Example/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace IpInfo.Example
+{
+    internal static class Program
+    {
+        private static async Task Main()
+        {
+            using var client = new HttpClient();
+            var api = new IpInfoApi(client);
+
+            var response = await api.GetCurrentInformationAsync();
+
+            Console.WriteLine($"City: {response.City}");
+        }
+    }
+}
