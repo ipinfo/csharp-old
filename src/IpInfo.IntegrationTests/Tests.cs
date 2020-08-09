ed7859c58bb4315b2c3947a0d5b44d03a8692acb
Konstantin S <havendv@gmail.com>
2020-08-09 21:25:16 +1000

    Refactored tests.

--- src/IpInfo.IntegrationTests/Tests.cs
+++ src/IpInfo.IntegrationTests/Tests.cs
@@ -15,21 +15,29 @@ namespace IpInfo.IntegrationTests
             using var source = new CancellationTokenSource(TimeSpan.FromSeconds(10));
             var cancellationToken = source.Token;
 
-            var token = Environment.GetEnvironmentVariable("IPINFO_TOKEN") ?? 
+            await ApiTestAsync(async api =>
+            {
+                var response = await api.GetCurrentIpInfoAsync(cancellationToken)
+                    .ConfigureAwait(false);
+
+                Assert.IsNotNull(response, nameof(response));
+
+                foreach (var property in response.GetType().GetProperties())
+                {
+                    Console.WriteLine($"{property.Name}: {property.GetValue(response)}");
+                }
+            });
+        }
+
+        private static async Task ApiTestAsync(Func<IpInfoApi, Task> action)
+        {
+            var token = Environment.GetEnvironmentVariable("IPINFO_TOKEN") ??
                         throw new InvalidOperationException("token is null.");
 
             using var client = new HttpClient();
             var api = new IpInfoApi(token, client);
 
-            var response = await api.GetCurrentIpInfoAsync(cancellationToken)
-                .ConfigureAwait(false);
-
-            Assert.IsNotNull(response, nameof(response));
-
-            foreach (var property in response.GetType().GetProperties())
-            {
-                Console.WriteLine($"{property.Name}: {property.GetValue(response)}");
-            }
+            await action(api).ConfigureAwait(false);
         }
     }
 }
